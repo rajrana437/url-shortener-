fdfc1430c14168dfd1166f267396c40633c97046
rajrana437 <rajrana437@gmail.com>
2024-12-08 12:32:23 -0500

    Change project name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -46,7 +46,7 @@ const HomePage = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-[#FFF7D1] to-[#FFECC8]">
       <div className="w-full max-w-lg p-8 bg-white shadow-lg rounded-xl border border-gray-200">
-        <h1 className="text-3xl font-bold text-[#FFB0B0] text-center mb-6">URL Shortener</h1>
+        <h1 className="text-3xl font-bold text-[#FFB0B0] text-center mb-6">ClipLink</h1>
         <div className="relative mb-6">
           <input
             type="text"
